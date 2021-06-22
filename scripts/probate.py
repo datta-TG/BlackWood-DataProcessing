7ab4fc4fc1bd40ddc26a8b677e370c1a73868725
Juanes Cendales <juanescendales@gmail.com>
2021-06-22 11:16:43 -0500

    Update commit
    Include Folio column to Probate.

--- scripts/probate.py
+++ scripts/probate.py
@@ -1,9 +1,10 @@
 #           Probate Script for transforming CSVs to only one table
 import os
 import pandas as pd
+import numpy as np
 
 def read_clean_csv(path):
-    main_columns = ['Case Number Probate','County','Filing Date']
+    main_columns = ['Case Number Probate','County','Filing Date','Folio']
     df = pd.read_csv(path)
     columns = df.columns.tolist()
     #Validation Case Number
@@ -11,6 +12,11 @@ def read_clean_csv(path):
     matches = list(set(columns) & set(other_case_number_names))
     if(len(matches)==1):
         df.rename(columns={matches[0]:'Case Number Probate'}, inplace=True)
+    # Validation Folio
+    other_filing_date_names = ['Folio']
+    matches = list(set(columns) & set(other_filing_date_names))
+    if(len(matches)==1):
+        df['Folio'] = df['Folio'].astype(str).str.replace('-','')
     # County integration"""
     df['County'] = 'Miami Dade'
     return df[main_columns]
@@ -22,5 +28,8 @@ if(len(list_of_files)>0):
     df = read_clean_csv(os.path.join(path_to_files,list_of_files[0]))
     for i in range(1,len(list_of_files)):
         df = pd.concat([df,read_clean_csv(os.path.join(path_to_files,list_of_files[i]))])
-    print(df.info())
+    
+
+df = df.replace('nan',np.nan, regex=True)
+print(df.info())
 df.to_csv(path_output,index=False)
